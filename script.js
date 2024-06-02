f4d3073e2b51e4e81f61f93077f7a1e0ebb754e6
Ismail Noori <ismailafghan1953@gmail.com>
2024-06-02 09:03:17 +0500

    I do chapters

---
+++ script.js
@@ -0,0 +1,24 @@
+// var num=+("Enter a number");
+
+// if (num%2==0){
+//     alert(num+"Number is even")
+// }
+// else{
+//     alert(num+"Number is odd")
+// }
+
+
+var trafficlight = prompt("Traffic light colour");
+
+if (trafficlight === "Red") {
+    alert("stop");
+}
+else if (trafficlight === "Yellow") {
+    alert("Be ready");
+}
+
+else {
+    alert("Go");
+}
+
+
